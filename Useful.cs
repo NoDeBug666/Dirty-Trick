42e736e2b68231bda5781545b6bfd42782546fa3
Watermelon Lee <pow75goof@gmail.com>
2016-05-15 06:26:40 +0800

    Update Useful.cs

--- Useful.cs
+++ Useful.cs
@@ -3,4 +3,9 @@
 a^(1/n)
 需要注意"項次"的部分,被除數ㄧ定要轉浮點,用整數處理會永遠為0
 */
-double result = Math.Pow( Input , (1.0 / 8) );
+double result0 = Math.Pow( Input , (1.0 / 8) );
+
+/*
+要取顯示某數到小數點第N(2)位,可以寫
+*/
+string result1 = String.Format("{0:2F}",Data);
