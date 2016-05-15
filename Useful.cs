c0d9f0eb6d82bef390263c0362319eecda18a2b8
Watermelon Lee <pow75goof@gmail.com>
2016-05-15 09:10:53 +0800

    Update Useful.cs

--- Useful.cs
+++ Useful.cs
@@ -12,3 +12,8 @@ double result0 = Math.Pow( Input , (1.0 / 8) );
 double Data;
 string result1 = String.Format("{0:F2}",Data);
 string result2 = Data.ToString("0.00");
+
+/*
+在C#中使用部件 DataGridView ,想要限制其Column不能排序
+*/
+this.dataGridView1.Columns[0].SortMode = DataGridViewColumnSortMode.NotSortable;
